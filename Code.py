4bfff4d7cfc943657c98aba8bfc0cbe96a4a3d8b
Thatsnotme1774 <135779760+Thatsnotme1774@users.noreply.github.com>
2024-02-29 20:18:50 +1100

    Updated Code.py
    the image still doesnt work but ill eventually fix it

--- Code.py
+++ Code.py
@@ -1,32 +1,39 @@
-
-#Importing modules
 from tkinter import *
 import customtkinter as ctk
 import pyttsx3
 import googletrans
 from textblob import*
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
 
 #Variables 
 geo = '500x600'
 Main_name = "Project"
 
-
-#Setting up Tkinter
 Window = ctk.CTk()
 Window.title(Main_name)
 Window.geometry(geo)
-#Grid Configuration
-Grid.rowconfigure(Window,0,weight=1) #automatically weight is set to 0, so if there is any extra room it won't fill but it will now fill because its set to 1
-Grid.columnconfigure(Window,0,weight=1)
-Grid.rowconfigure(Window,1,weight=1)
+
+#frame
+
 #Buttons
-Button1 = ctk.CTkButton(Window, text="Button1")
-Button2 = Button(Window, text="button2")
-Button1.grid(row=0,column=0,sticky="NSEW") #Sticky = NSEW allows it dynamically resize itself
-Button2.grid(row=1,column=0,sticky="NSEW")
 
 
 
+def resize_image(event):
+    new_width = event.width
+    new_height = event.height
+    image = copy_of_image.resize((new_width, new_height))
+    photo = ctk.CTkImage(image)
+    label.config(image = photo)
+    label.image = photo #avoid garbage collection
+
+image = Image.open('Assignment\download.png')
+copy_of_image = image.copy()
+photo = ctk.CTkImage(image)
+label = ctk.CTkLabel(Window, image = photo)
+label.bind('<Configure>', resize_image)
+label.pack(fill=BOTH, expand = YES)
 
 Window.mainloop()
-
