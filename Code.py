7f27f803fd1c71a27f33aae5c2a25a1f42017086
Thatsnotme1774 <135779760+Thatsnotme1774@users.noreply.github.com>
2024-05-01 12:29:51 +1000

    Background_works.py
    The background finally works and the code opens in fullscreen

--- Code.py
+++ Code.py
@@ -1,39 +1,30 @@
+
 from tkinter import *
-import customtkinter as ctk
-import pyttsx3
-import googletrans
-from textblob import*
-from tkinter import PhotoImage
+from tkinter import ttk
 from PIL import Image, ImageTk
+import customtkinter
 
+root = customtkinter.CTk()
+root.title("Title")
+root.overrideredirect(True)
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
-#Variables 
-geo = '500x600'
-Main_name = "Project"
-
-Window = ctk.CTk()
-Window.title(Main_name)
-Window.geometry(geo)
-
-#frame
-
-#Buttons
-
-
-
+#fUNCTIONS
 def resize_image(event):
+    global new_height, new_width
     new_width = event.width
     new_height = event.height
     image = copy_of_image.resize((new_width, new_height))
-    photo = ctk.CTkImage(image)
+    photo = ImageTk.PhotoImage(image)
     label.config(image = photo)
-    label.image = photo #avoid garbage collection
+    label.image = photo
 
 image = Image.open('Assignment\download.png')
 copy_of_image = image.copy()
-photo = ctk.CTkImage(image)
-label = ctk.CTkLabel(Window, image = photo)
+photo = ImageTk.PhotoImage(image)
+label = ttk.Label(root, image = photo)
 label.bind('<Configure>', resize_image)
 label.pack(fill=BOTH, expand = YES)
 
-Window.mainloop()
+
+root.mainloop()
